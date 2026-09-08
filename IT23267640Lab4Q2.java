2ea4cac909827ce3ce85205806f415b9ec499771
Bishan Wishvajith <167428653+wishvajithW3@users.noreply.github.com>
2026-09-08 10:20:45 +0530

    Fix class name and correct percentage prompts

--- IT23267640Lab4Q2.java
+++ IT23267640Lab4Q2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class IT26101660Lab4Q2{
+public class IT23267640Lab4Q2{
 	public static void main(String[] args){
 		double examMarks, labsMarks, examPercentage, LabPercentage, totalExamMarks;
 		Scanner input= new Scanner(System.in);
