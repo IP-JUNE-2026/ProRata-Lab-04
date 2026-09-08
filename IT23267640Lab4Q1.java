0e8fe2e22cebba90b57158e08cb4d4e36acca380
Bishan Wishvajith <167428653+wishvajithW3@users.noreply.github.com>
2026-09-08 10:20:21 +0530

    Refactor class name to match filename

--- IT23267640Lab4Q1.java
+++ IT23267640Lab4Q1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- public class IT26101660Lab4Q1{
+ public class IT23267640Lab4Q1{
 	 public static void main(String[] args){
 		 
 		Scanner input = new Scanner(System.in);
